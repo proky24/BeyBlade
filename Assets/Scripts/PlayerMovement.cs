655f111ce34e7680ea91dd6f2f1ab9d68e6f9e3b
Jakub <kjakub.dev@gmail.com>
2025-10-02 00:12:25 +0200

    zmena projektu na verzi 6.2.6f1 a doladeni PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using UnityEngine;
+public enum SpinMode
+{
+    Spinning = 1,
+    NotSpinning = 0
+}
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Body")] 
+    [Header("Body/Komponenty")] 
     [SerializeField] private GameObject body;
+    [SerializeField] private Rigidbody rb;
     [Header("Vlastnosti")]
     [SerializeField] private float movementSpeed = 10f;
-    [SerializeField] private float rotationSpeed = 360f; // Degrees per second
+    [SerializeField] private float degreesPerSec = 360f;
     [SerializeField] private float power = 0f;
-    [SerializeField] private float powerGainMultiplier = 1f;
     [SerializeField] private float maxPower = 2f;
-    
-    private bool isSpinning = false;
+    [SerializeField] private float powerGainMultiplier = 1f;
+    private SpinMode spinMode = SpinMode.NotSpinning;
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Space) && !isSpinning)
+        if (Input.GetKey(KeyCode.Space) && spinMode == SpinMode.NotSpinning)
         {
             power += Time.deltaTime * powerGainMultiplier;
+
             if (power > maxPower)
             {
                 power = maxPower;
@@ -24,37 +30,35 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            if (power > 0f && !isSpinning)
+            if (power > 0f && spinMode == SpinMode.NotSpinning)
             {
-                StartCoroutine(SpinAndMove());
+                StartCoroutine(SpinAndMove(degreesPerSec, power, spinMode));
+                power = 0f;
             }
         }
     }
-    private IEnumerator SpinAndMove()
+    private IEnumerator SpinAndMove(float rotationSpeed, float currentPower, SpinMode playerSpinMode)
     {
-        isSpinning = true;
-
-        float duration = power;
-        float elapsed = 0f;
+        playerSpinMode = SpinMode.Spinning;
+        var duration = currentPower;
+        var elapsed = 0f;
+        var screenPos = Input.mousePosition;
+        screenPos.z = 20f;
 
         while (elapsed < duration)
         {
+            //rotaci by nejspis mela delat animace
             body.transform.Rotate(Vector3.up, rotationSpeed * (duration - elapsed) * Time.deltaTime, Space.Self);
             elapsed += Time.deltaTime;
-            //Move();
+            MoveTowards(Camera.main.ScreenToWorldPoint(screenPos));
             yield return null;
         }
 
-        power = 0f;
-        isSpinning = false;
+        playerSpinMode = SpinMode.NotSpinning;
         yield return null;
     }
-
-    private void Move()
+    private void MoveTowards(Vector3 positionToMoveTowards)
     {
-        transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
-        //TODO: udelat pohyb k mysi
+        rb.AddForce(new Vector3(positionToMoveTowards.x, gameObject.transform.position.y, positionToMoveTowards.z) * movementSpeed * Time.deltaTime);
     }
 }
-
-
