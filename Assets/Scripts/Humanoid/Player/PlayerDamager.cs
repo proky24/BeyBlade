f279bc3923b11e6c25079a2bd142ed6852281b2b
Jakub <kjakub.dev@gmail.com>
2025-10-15 09:14:37 +0200

    playerdamager.cs zmena detekce
    misto kolize trigger

--- Assets/Scripts/Humanoid/Player/PlayerDamager.cs
+++ Assets/Scripts/Humanoid/Player/PlayerDamager.cs
@@ -4,9 +4,9 @@ public class PlayerDamager : MonoBehaviour
     [Header("Vlastnosti")]
     [SerializeField]
     private float damage = 10f;
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.collider.TryGetComponent<IDamageable>(out var dmg))
+        if (other.TryGetComponent<IDamageable>(out var dmg))
         {
             dmg.Damage(damage);
         }
